96483ca02c1b5321b9821ed7c0e18075ab228b38
Роман Кодочигов <kodochigovra@mail.ru>
2022-02-14 19:32:03 +0300

    add two models, media_root, admin panel

--- mysite/mainapp/models.py
+++ mysite/mainapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=64,
+                            unique=True)
+    is_active = models.BooleanField(verbose_name='активна', default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='Наименование', max_length=64)
+    description = models.TextField(verbose_name='Описание товара',
+                                   blank=True)
+    short_desc = models.CharField(verbose_name='Краткое описание',
+                                  max_length=64, blank=True)
+    image = models.ImageField(verbose_name='Изображение',
+                              upload_to='products_images', blank=True)
+    price = models.DecimalField(verbose_name='Цена', max_digits=8,
+                                decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name='Кол-во на складе',
+                                           default=0)
+    is_active = models.BooleanField(verbose_name='активна', default=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.category.name}"
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 import mainapp.views as mainapp
 
 urlpatterns = [
@@ -22,3 +25,7 @@ urlpatterns = [
     path('products', mainapp.products, name='products'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)

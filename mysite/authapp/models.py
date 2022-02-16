7f8c0842dbc5db914179d95e2cc397b57d51e782
Роман Кодочигов <kodochigovra@mail.ru>
2022-02-16 22:15:41 +0300

    add authapp for login and registration

---
+++ mysite/authapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class ShopUser(AbstractUser):
+    age = models.PositiveIntegerField(verbose_name='возраст', blank=True,
+                                      default=18)
---
+++ mysite/authapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views as authapp
+
+app_name = 'authapp'
+
+urlpatterns = [
+    path('login/', authapp.login, name='login'),
+    path('logout/', authapp.logout, name='logout'),
+    path('edit/', authapp.edit, name='edit'),
+    path('register/', authapp.register, name='register')
+]
